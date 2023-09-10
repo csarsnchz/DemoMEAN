80760c69e64b55fa3d07b178e8548d3be77555d8
csarsnchz <csarsnchz@gmail.com>
2023-09-10 18:25:15 +0000

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+'use strict'
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const port = process.env['PORT'] || 4301;
+const uri = process.env['MONGO_URI'];
+
+var cliente_route = require('./routes/cliente');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json({ limit: '50mb', extended: true }))
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Allow', 'GET, PUT, POST, DELETE, OPTIONS');
+  next();
+});
+
+app.use('/api', cliente_route);
+
+module.exports = app;
