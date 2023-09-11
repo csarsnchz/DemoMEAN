c85f340f75cf81b74f067f5d087ba1ef11728b3a
csarsnchz <csarsnchz@gmail.com>
2023-09-11 00:31:30 +0000

    Added Angular Initial Conf
    Added Angular Initial Conf

---
+++ admin/src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+
+const appRoute : Routes [
+  {path: '', component: inicioComponent}
+]
+
+export const appRoutingProviders :  any[]=[];
+export const routing ModuleWithProviders<any> = RouterModule.forRoot(appRoute);
