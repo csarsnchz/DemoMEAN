83f497b203ee6ff7aa24d023543518d59ef7aa86
csarsnchz <csarsnchz@gmail.com>
2023-09-22 04:15:29 +0000

    Added Index Client Component

--- admin/src/app/app.routing.ts
+++ admin/src/app/app.routing.ts
@@ -1,12 +1,19 @@
 import { Routes, RouterModule } from "@angular/router";
 import { ModuleWithProviders } from "@angular/core";
 import { inicioComponent } from "./components/inicio/inicio.component";
+import { LoginComponent } from "./components/login/login.component";
 
-import { AdminGuard } from "./guards/admin.guard";
+import { adminGuard } from "./guards/admin.guard";
+import { IndexClienteComponent } from "./components/clientes/index-cliente/index-cliente.component";
 
 const appRoute: Routes[
   { path: '', component: inicioComponent, canActivate: [AdminGuard]	},
-  { path: 'login', component: loginComponent }
+  
+  { path 'panel', children: [
+    {path 'clientes', component: IndexClienteComponent, CanActivateFn: [ adminGuard]}
+  ]},
+  
+  { path: 'login', component: LoginComponent }
 ]
 
 export const appRoutingProviders: any[] = [];
