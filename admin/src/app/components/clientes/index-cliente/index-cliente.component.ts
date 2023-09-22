7bafaca623c25054eee3b4de2b17eb64bb678bc8
csarsnchz <csarsnchz@gmail.com>
2023-09-22 04:45:20 +0000

    Added Cliente Service

--- admin/src/app/components/clientes/index-cliente/index-cliente.component.ts
+++ admin/src/app/components/clientes/index-cliente/index-cliente.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { error } from 'console';
+import { response } from 'express';
+import { ClienteService } from 'src/app/services/cliente.service';
 
 @Component({
   selector: 'app-index-cliente',
   templateUrl: './index-cliente.component.html',
   styleUrls: ['./index-cliente.component.css']
 })
-export class IndexClienteComponent {
+export class IndexClienteComponent implements OnInit{
+
+  public clientes : Array<any> = [];
+
+  constructor(
+    private _clienteService : ClienteService 
+  ){}
+
+  ngOnInit(): void {
+    this._clienteService.listar_clientes_filtro_admin().subscribe(
+      response=>{
+        console.log(response);
+      },
+      error=>{
+        console.log(error);
+      }
+    )
+  }
 
 }
