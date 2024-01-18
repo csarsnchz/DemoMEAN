b1fa0d36ed566281e40e8bfdac42f6d26c76c208
csarsnchz <csarsnchz@gmail.com>
2024-01-18 18:09:16 +0000

    Add CreateProductoComponent to app.module and app.routing

---
+++ admin/src/app/components/productos/create-producto/create-producto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-producto',
+  templateUrl: './create-producto.component.html',
+  styleUrls: ['./create-producto.component.css']
+})
+export class CreateProductoComponent {
+
+}
